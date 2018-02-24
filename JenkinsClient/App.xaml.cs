e8c6bc0e9d9b44a98b803ff7aefd97adac5a16fc
Unknown <jonathan-harwood@outlook.com>
2018-02-24 18:54:06 +0000

    Initial project commit
    - Added NuGet packages for project - FontAwesome, Newtonsoft.JSON, StyleCop and Castle.Windsor.
    - Initial code for MainWindow.xaml(.cs) and App.xaml(.cs).
    - Fixed initial StyleCop errors, and added some suppressions.

---
+++ JenkinsClient/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace JenkinsClient
+{
+    using System.Windows;
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static IWindsorContainer ioc = new WindsorContainer();
+
+        public static IWindsorContainer IOC
+        {
+            get
+            {
+                return ioc;
+            }
+
+            set
+            {
+                ioc = value;
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ioc.Install(FromAssembly.This());
+        }
+    }
+}
