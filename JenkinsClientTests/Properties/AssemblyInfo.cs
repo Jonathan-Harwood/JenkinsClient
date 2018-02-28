4b0e341bf2121163680ce6595546c86bec83884e
Unknown <jonathan-harwood@outlook.com>
2018-02-28 18:21:31 +0000

    Create JenkinsClientTests project
    - Created project in the correct format: .NET Framework.
    - Added JenkinsClient reference to project.
    - Added NuGet packages: StyleCop, Moq and FluentAssertions.
    - Created GlobalSuppressions file to defend against certain unnecessary StyleCop enforcements.

---
+++ JenkinsClientTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JenkinsClientTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonathan Harwood")]
+[assembly: AssemblyProduct("JenkinsClientTests")]
+[assembly: AssemblyCopyright("Copyright © Jonathan Harwood 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ecd28e42-0be5-4773-b725-63cf8b145b38")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
