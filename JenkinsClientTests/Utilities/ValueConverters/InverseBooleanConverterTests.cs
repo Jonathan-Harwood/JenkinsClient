943bdfc42b82a121de5d0e5c9a993f041729ad22
Unknown <jonathan-harwood@outlook.com>
2018-04-03 19:15:25 +0100

    Create InverseBoolean value converter
    - Boolean takes a boolean value and returns the inverse of it.
    - ConvertBack not implemented.
    - Unit tests covering the Convert method.

---
+++ JenkinsClientTests/Utilities/ValueConverters/InverseBooleanConverterTests.cs
@@ -0,0 +1,66 @@
+﻿namespace JenkinsClientTests.Utilities.ValueConverters
+{
+    using System;
+    using FluentAssertions;
+    using JenkinsClient.Utilities.ValueConverters;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InverseBooleanConverterTests
+    {
+        private InverseBooleanConverter inverseBooleanConverter = new InverseBooleanConverter();
+
+        #region Convert
+        [TestMethod]
+        public void InverseBooleanConverter_Convert_TrueValueSupplied_FalseReturned()
+        {
+            // Assemble
+            bool suppliedValue = true;
+
+            // Action
+            object returnedValue = inverseBooleanConverter.Convert(suppliedValue, null, null, null);
+
+            // Assert
+            returnedValue.Should().Be(false);
+        }
+
+        [TestMethod]
+        public void InverseBooleanConverter_Convert_FalseValueSupplied_TrueReturned()
+        {
+            // Assemble
+            bool suppliedValue = false;
+
+            // Action
+            object returnedValue = inverseBooleanConverter.Convert(suppliedValue, null, null, null);
+
+            // Assert
+            returnedValue.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void InverseBooleanConverter_Convert_StringValueSupplied_ThrowsInvalidCastException()
+        {
+            // Assemble
+            string suppliedValue = "InvalidParameter";
+
+            // Action
+            Action convertAction = () => inverseBooleanConverter.Convert(suppliedValue, null, null, null);
+
+            // Assert
+            convertAction.Should().Throw<InvalidCastException>();
+        }
+        #endregion Convert
+
+        #region ConvertBack
+        [TestMethod]
+        public void InverseBooleanConverter_ConvertBack_ThrowsNotImplementedException()
+        {
+            // Action
+            Action convertBackAction = () => inverseBooleanConverter.ConvertBack(null, null, null, null);
+
+            // Assert
+            convertBackAction.Should().Throw<NotImplementedException>();
+        }
+        #endregion ConvertBack
+    }
+}
