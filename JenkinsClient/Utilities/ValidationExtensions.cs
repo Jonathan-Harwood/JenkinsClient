e14e781650016deeafb14b4cb929ba1ce286e618
Unknown <jonathan-harwood@outlook.com>
2018-04-07 23:07:14 +0100

    Add EnsureIsNotNull helper method
    - EnsureIsNotNull throws ArgumentNullException in the event of a null parameter passed in.

---
+++ JenkinsClient/Utilities/ValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace JenkinsClient.Utilities
+{
+    using System;
+
+    public static class ValidationExtensions
+    {
+        public static void EnsureIsNotNull<T>(this T parameter, string name = null)
+            where T : class
+        {
+            if (parameter == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
---
+++ JenkinsClientTests/Utilities/ValidationExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿namespace JenkinsClientTests.Utilities
+{
+    using System;
+    using FluentAssertions;
+    using JenkinsClient.Utilities;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ValidationExtensionsTests
+    {
+        #region EnsureIsNotNull
+        [TestMethod]
+        public void ValidationExtensions_EnsureIsNotNull_NullParameter_ThrowsArgumentNullException()
+        {
+            // Assemble
+            string appSettingsParameter = null;
+            string appSettingsMessage = "appSettings";
+            string exceptionMessage = $"Value cannot be null.\nParameter name: {appSettingsMessage}";
+
+            // Action
+            Action ensureIsNotNullAction = () => ValidationExtensions.EnsureIsNotNull(appSettingsParameter, appSettingsMessage);
+
+            // Assert
+            ensureIsNotNullAction.Should().Throw<ArgumentNullException>().WithMessage(exceptionMessage);
+        }
+
+        [TestMethod]
+        public void ValidationExtensions_EnsureIsNotNull_NonNullParameter_DoesNotThrowArgumentNullException()
+        {
+            // Assemble
+            string appSettingsParameter = "appSettings";
+            string appSettingsMessage = "appSettings";
+
+            // Action
+            Action ensureIsNotNullAction = () => ValidationExtensions.EnsureIsNotNull(appSettingsParameter, appSettingsMessage);
+
+            // Assert
+            ensureIsNotNullAction.Should().NotThrow<ArgumentNullException>();
+        }
+        #endregion EnsureIsNotNull
+    }
+}
